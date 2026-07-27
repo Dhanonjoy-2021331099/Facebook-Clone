92366f6c4731925b4536e81ac966a648f0531466
Dhanonjoy Kumar Barman <2021331099@student.sust.edu>
2026-07-27 10:34:20 +0600

    Setup MongoDB configuration

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("Backend is Running 🚀");
 });
 
-// Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
